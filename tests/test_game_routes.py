299b8e53d978ef30109f1dc0d03496ffff58e4c5
jxz72 <jeffreyxzhou02@gmail.com>
2025-04-28 00:59:58 -0400

    fix up a unit test

--- tests/test_game_routes.py
+++ tests/test_game_routes.py
@@ -1,15 +1,16 @@
 from app.app import create_app
 
 class TestGameRoutes:
-    # This is using the pytest-mock mocker fixture
     def test_create_game_route(self, mocker):
         mock_create_game = mocker.patch('app.services.game_services.GameService.create_game')
-        mock_create_game.return_value = "65c83135-18a7-4556-9bd2-d15b77532133"
+        expected_game_id = "65c83135-18a7-4556-9bd2-d15b77532133"
+        mock_create_game.return_value = expected_game_id
         app = create_app()
 
         client = app.test_client()
         response = client.post('/create_game')
 
         mock_create_game.assert_called_once()
-    
-        assert response.data.decode() == "65c83135-18a7-4556-9bd2-d15b77532133"
+        
+        assert response.status_code == 200 
+        assert response.data.decode() == expected_game_id
